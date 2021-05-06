33e55a26635457e39147a3b10feb7b6b8010ec7a
jagtapnn0509 <jagtapnn05@gmail.com>
2021-05-07 01:56:25 +0530

    Add : function to manage Employee Wage For Multiple Company

---
+++ EmployeeWage/ComputeEmpWage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeeWage
+{
+    public interface IComputeEmpWage
+    {
+        void addCompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMnth);
+        void computeEmpWage();
+
+        int getTotalWage(string company);
+    }
+}
--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -6,12 +6,10 @@ namespace EmployeeWage
     {
         public static void Main(string[] args)
         {
-            EmployeeDetails google = new EmployeeDetails("Google", 20, 2, 10);
-            EmployeeDetails vipro = new EmployeeDetails("Vipro", 10, 4, 20);
-            google.calculateEmployee();
-            vipro.calculateEmployee();
-            Console.WriteLine(google.toString());
-            Console.WriteLine(vipro.toString());
+            EmpWageBuilder empWageBuilder = new EmpWageBuilder();
+            empWageBuilder.addCompanyEmpWage("DMart", 20, 2, 10);
+            empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
+            empWageBuilder.computeEmpWage();
         }
     }
 }
