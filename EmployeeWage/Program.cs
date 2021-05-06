c4ca5a4dc0ea389644136707860dacb02a389125
jagtapnn0509 <jagtapnn05@gmail.com>
2021-05-07 00:27:20 +0530

    Add:function to calculate Full Time Employee Wage

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -8,13 +8,17 @@ namespace EmployeeIsPresentorAbsent
         {
             Console.WriteLine("Welcome to the Employee Wage Computation Program");
             int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+            int empHrs = 0;
+            int empWage = 0;
             Random random = new Random();
             int empCheck = random.Next(0, 2);
             if (empCheck == IS_FULL_TIME)
-                Console.WriteLine("Employee Is Present");
-            else
-                Console.WriteLine("Employee Is Absent");
-            Console.ReadKey();
+            {
+                empHrs = 8;
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("The Full Time Employee Wage is :" + empWage);
         }
     }
 }
