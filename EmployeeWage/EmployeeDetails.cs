b5b2b8442ff2788a9157f6c698af05b6ff81d118
jagtapnn0509 <jagtapnn05@gmail.com>
2021-05-07 01:09:40 +0530

    Add : function to comute Multiple Company Employee Wage

---
+++ EmployeeWage/EmployeeDetails.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace EmployeeWage
+{
+    class EmployeeDetails
+    {
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
+
+
+        public static int calculateEmployee(string company, int empRatePerHour, int numOfWorkingDays, int maxhoursPerMonth)
+        {
+            Console.WriteLine("Welcome to the Employee Wage Computation Program using C#");
+            int totalEmpWage = 0;
+            int empHrs = 0;
+            int totalEmpHrs = 0;
+            int totalWorkingDays = 0;
+            while (totalEmpHrs <= maxhoursPerMonth && totalWorkingDays < numOfWorkingDays)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case 1:
+                        empHrs = 8;
+                        break;
+                    case 2:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day= " + totalWorkingDays + "Emp Hrs= " + empHrs);
+            }
+            totalEmpWage = totalEmpHrs * empRatePerHour;
+            Console.WriteLine("The total  Employee Wage for :" + company + "IS =" + totalEmpWage);
+            return totalEmpWage;
+        }
+    }
+}
+
--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -4,44 +4,10 @@ namespace EmployeeWage
 {
     class Program
     {
-        public const int IS_FULL_TIME = 1;
-        public const int IS_PART_TIME = 2;
-        public const int EMP_RATE_PER_HOUR = 20;
-        public const int NUM_OF_WORKING_DAYS = 20;
-        public const int MAX_HRS_IN_MONTH = 100;
-
-        public static void calculateEmployee()
-        {
-            Console.WriteLine("Welcome to the Employee Wage Computation Program using C#");
-            int totalEmpWage = 0;
-            int empHrs = 0;
-            int totalEmpHrs = 0;
-            int totalWorkingDays = 0;
-            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
-            {
-                totalWorkingDays++;
-                Random random = new Random();
-                int empCheck = random.Next(0, 3);
-                switch (empCheck)
-                {
-                    case 1:
-                        empHrs = 8;
-                        break;
-                    case 2:
-                        empHrs = 4;
-                        break;
-                    default:
-                        empHrs = 0;
-                        break;
-                }
-                totalEmpHrs += empHrs;
-            }
-            totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("The total Monthly Employee Wage is :" + totalEmpWage);
-        }
         public static void Main(string[] args)
         {
-            calculateEmployee();
+            EmployeeDetails.calculateEmployee("Google", 20, 5, 20);
+            EmployeeDetails.calculateEmployee("TCS", 30, 5, 20);
         }
     }
 }
